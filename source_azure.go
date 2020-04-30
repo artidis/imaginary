cc780b8733d2b14ebe88011d665dcec62f42e26a
Antonio Bosak <toni.bosak@gmail.com>
2020-04-30 12:14:09 +0200

    added loging through azure oauth

--- source_azure.go
+++ source_azure.go
@@ -7,24 +7,30 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/Azure/go-autorest/autorest/azure"
 )
 
 const ImageSourceTypeAzure ImageSourceType = "azure"
 
+var (
+	credential azblob.TokenCredential
+)
+
 func init() {
+	if err := initAzure(); err != nil {
+		panic(err.Error())
+	}
+
 	RegisterSource(ImageSourceTypeAzure, NewAzureImageSource)
 }
 
 func newAzureSession(container string) (*azblob.ContainerURL, error) {
 	accountName := os.Getenv("AZURE_ACCOUNT_NAME")
-	accountKey := os.Getenv("AZURE_ACCOUNT_KEY")
-
-	credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
-	if err != nil {
-		return nil, fmt.Errorf("azure: credential error: %w", err)
-	}
 
 	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
 	u, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net", accountName))
@@ -101,4 +107,63 @@ func parseAzureBlobOutputKey(request *http.Request) string {
 
 func parseAzureContainer(request *http.Request) string {
 	return request.URL.Query().Get("azureContainer")
+}
+
+func initAzure() error {
+	azureEnv, err := azure.EnvironmentFromName("AZUREPUBLICCLOUD")
+	if err != nil {
+		return fmt.Errorf("azure/init: error getting environment from name: %s", err)
+	}
+
+	azureTenantID := os.Getenv("AZURE_TENANT_ID")
+	azureOAuthConfig, err := adal.NewOAuthConfig(azureEnv.ActiveDirectoryEndpoint, azureTenantID)
+	if err != nil {
+		return fmt.Errorf("azure/init: error in new oauth config: %s", err)
+	}
+
+	if azureOAuthConfig.IsZero() {
+		return fmt.Errorf("azure/init: error configuring oauth for tenant")
+	}
+
+	azureClientID := os.Getenv("AZURE_CLIENT_ID")
+	azureClientSecret := os.Getenv("AZURE_CLIENT_SECRET")
+
+	spt, err := adal.NewServicePrincipalToken(
+		*azureOAuthConfig,
+		azureClientID,
+		azureClientSecret,
+		azureEnv.ResourceIdentifiers.Storage,
+	)
+	if err != nil {
+		return fmt.Errorf("azure: error getting service principal auth: %w", err)
+	}
+
+	tokenRefresher := func(spt *adal.ServicePrincipalToken) func(credential azblob.TokenCredential) time.Duration {
+		lock := sync.Mutex{}
+		return func(credential azblob.TokenCredential) time.Duration {
+			// This is possible data race for token refresh so we lock it down.
+			lock.Lock()
+			defer lock.Unlock()
+
+			if err := spt.Refresh(); err != nil {
+				panic(fmt.Errorf("azure: error refreshing token: %s", err))
+			}
+
+			token := spt.Token()
+			credential.SetToken(token.AccessToken)
+
+			expiresIn, err := token.ExpiresIn.Int64()
+			if err != nil {
+				panic(err)
+			}
+
+			// We reduce the given time by 2 minutes so instead that token is active 10min we
+			// refresh it after 8.
+			return time.Duration(expiresIn-2*60) * time.Second
+		}
+	}(spt)
+
+	credential = azblob.NewTokenCredential("", tokenRefresher)
+
+	return nil
 }
